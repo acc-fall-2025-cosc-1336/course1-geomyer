e6b790875b7f11e0bf0d18821f9ec9b0f340246e
geomyer <geoffrey.myers@g.austincc.edu>
2025-10-02 20:28:21 -0500

    Add files via upload

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,9 @@
 import unittest
 '''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
+the file in tests/homework/c_decisions/tests_decisions.py 
 has the test functions
 '''
-from tests.homework.b_in_proc_out import tests_in_proc_out
+from tests.homework.c_decisions import tests_decisions as main
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
+suite = unittest.TestLoader().loadTestsFromModule(main)
 unittest.TextTestRunner(verbosity=2).run(suite)
