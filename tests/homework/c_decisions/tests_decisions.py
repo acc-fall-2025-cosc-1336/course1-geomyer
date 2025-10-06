b82aaed3320d2387c7bb10ee4bf035c2ac0e7865
geomyer <geoffrey.myers@g.austincc.edu>
2025-10-06 17:55:31 +0000

    HW4 Menu added

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,19 @@
-#
+##Test case for homework c_decisions.py
+import unittest
+
+from src.homework.d_repetition import get_factorial, sum_odd_numbers
+
+class TestDecisions(unittest.TestCase):
+
+    def test_get_factorial(self):
+        self.assertEqual(get_factorial(0), 1)
+        self.assertEqual(get_factorial(1), 1)
+        self.assertEqual(get_factorial(5), 120)
+        self.assertEqual(get_factorial(10), 3628800)
+
+    def test_sum_odd_numbers(self):
+        self.assertEqual(sum_odd_numbers(1), 1)
+        self.assertEqual(sum_odd_numbers(2), 1)
+        self.assertEqual(sum_odd_numbers(5), 9)
+        self.assertEqual(sum_odd_numbers(10), 25)
+ 
