c32e172ee0034ec7c18690110bd83f42846ddc74
geomyer <geoffrey.myers@g.austincc.edu>
2025-11-11 00:37:25 +0000

    HW 07 with Main

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,18 @@
-#
+#Homework on Lists and Tuples. get lowest list values no magic methods
+
+def get_lowest_values(input_list, n):
+    nums=sorted(input_list)
+    lowest_nums=[]
+    for i in range(n):
+        lowest_nums.append(nums[i])
+    return lowest_nums
+print(get_lowest_values([8,10,1,50,20],3))
+
+
+def get_highest_values(input_list, n):
+    nums=sorted(input_list, reverse=True)
+    highest_nums=[]
+    for i in range(n):
+        highest_nums.append(nums[i])
+    return highest_nums
+print(get_highest_values([8,10,1,50,20],2))
--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,31 @@
 #
+#Test Menu for to show the list of lowest and high values from a list
+from unittest.main import main
+from lists import get_lowest_values, get_highest_values
+
+
+# Menu of lowest and highest values. Prompt user for input Options: 1. Get lowest and highest values from the list 2. Exit
+def display_menu(lowest, highest):  
+    while True:
+        print("Menu")
+        print("1. Get lowest and highest values from the list")
+        print("2. Exit")
+        choice = input("Enter your choice (1-2): ")
+        if choice == '1':
+            print(f"The lowest values are: {lowest}")
+            print(f"The highest values are: {highest}")
+        elif choice == '2':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+def main():
+    input_list = [8, 10, 1, 50, 20]
+    n_lowest = 3
+    n_highest = 2
+    lowest_values = get_lowest_values(input_list, n_lowest)
+    highest_values = get_highest_values(input_list, n_highest)
+    display_menu(lowest_values, highest_values)
+if __name__ == "__main__":
+    main()  
