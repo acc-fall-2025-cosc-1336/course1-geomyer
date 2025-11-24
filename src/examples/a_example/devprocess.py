b2c76ca4191216e43f5e7191561d14ace3e7122c
geomyer <geoffrey.myers@g.austincc.edu>
2025-11-24 00:15:43 +0000

    HW9

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -1,3 +1,8 @@
 def add_numbers(val1, val2):
      '''Type the correct code to add two numbers after the return statement.'''
      return val1 + val2
+
+
+def print_hello(name):
+    '''Type the correct code to print a greeting message after the return statement.'''
+    return f"Hello, {name}!
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,25 @@
-#
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
+
+class Test_Config(unittest.TestCase):
+
+    def setUp(self):
+        self.widgets = {}
+
+    def test_add_inventory(self):
+        add_inventory(self.widgets, 'Widget1', 10)
+        self.assertIn('Widget1', self.widgets)
+        self.assertEqual(self.widgets['Widget1'], 10)
+#Test case test_remove_inventory_widget
+#Write a test case add two widgets(widget1 and widget2 with quantities of your choice).
+#Remove widget1. Test that the length of dictionary is 1 and that widget2 still exists. 
+
+def test_remove_inventory_widget(self):
+        add_inventory(self.widgets, 'Widget1', 10)
+        add_inventory(self.widgets, 'Widget2', 20)
+        remove_inventory_widget(self.widgets, 'Widget1', 10)
+        self.assertNotIn('Widget1', self.widgets)
+        self.assertIn('Widget2', self.widgets)
+        self.assertEqual(len(self.widgets), 1)
+        
